9d881fef9ccbb8a3f4772111d35305ae9445b4fb
23-jose <jose.miguel.b.antunes@gmail.com>
2024-10-24 19:31:19 +0900

    commit message???

--- frontend/app/map/_components/LocationCard.tsx
+++ frontend/app/map/_components/LocationCard.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import Link from "next/link";
+import {FaHeart} from 'react-icons/fa';
+
 export default function LocationCard({
   title,
   photo,
@@ -10,16 +15,17 @@ export default function LocationCard({
   address: string;
 }) {
   return (
-    <div className="flex flex-col border-2">
-      <div className="flex justify-between">
-        <p>{title}</p>
-        <div>favorite</div>
-      </div>
-      <div>
-        <div>{photo}</div>
-        <p>{description}</p>
-        <p>{address}</p>
-      </div>
-    </div>
+    <Link href={"/"} passHref>
+      <div className="verticle justify-left"></div>
+        <div className="flex justify-between">
+          <p className="text-lg font-bold pb-4 pl-1">{title}</p>
+          <div>
+            <FaHeart className="text-red-500 h-10 w-10 pr-4"/>
+          </div>
+        </div>
+        <div className="w-full rounded-[12px] bg-neutral-200 p-4 min-h-[200px]">{photo}</div>
+        <p className="w-full pt-4 p-2">{description}</p>
+        <p className="w-full text-sm text-gray-500 p-2">{address}</p>
+    </Link>
   );
 }
