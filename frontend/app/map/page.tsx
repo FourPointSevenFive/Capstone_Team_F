5a4bb04353c4321be54328724f73f754087cd3b7
youznn <leejin914418@gmail.com>
2024-11-06 22:06:40 +0900

    feature(fe) - implement bottom sheet

--- frontend/app/map/page.tsx
+++ frontend/app/map/page.tsx
@@ -1,16 +1,18 @@
+"use client";
+
 import Header from "@/app/_components/Header";
 import SearchBar from "../_components/SearchBar";
 import LocationCard from "./_components/LocationCard";
 import CustomBadge from "../_components/CustomBadge";
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { BsChevronDoubleUp } from "react-icons/bs";
 
 export default function Page() {
   return (
@@ -20,63 +22,71 @@ export default function Page() {
         <CustomBadge title="BTS" category="kpop" />
         <SearchBar />
       </div>
-      <div className="flex flex-col gap-3">
-        <div>
-          <LocationCard
-            title="Bongsuyuk"
-            photo="photohere"
-            description="mookup"
-            address="~~ "
-          />
-          <LocationCard
-            title="skku"
-            photo="photo"
-            description="blabla"
-            address="somewhere"
-          />
-        </div>
-
-        <Sheet>
-          <SheetTrigger>trigger</SheetTrigger>
-          <SheetHeader></SheetHeader>
-          <SheetTitle>Sheet Title</SheetTitle>
-          <SheetDescription>Sheet Description</SheetDescription>
-          <SheetContent side="bottom">
-            <ScrollArea className="h-[90vh]">
-              <LocationCard
-                title="skku"
-                photo="photo"
-                description="blabla"
-                address="somewhere"
-              />
-              <LocationCard
-                title="skku"
-                photo="photo"
-                description="blabla"
-                address="somewhere"
-              />
-              <LocationCard
-                title="skku"
-                photo="photo"
-                description="blabla"
-                address="somewhere"
-              />
-              <LocationCard
-                title="skku"
-                photo="photo"
-                description="blabla"
-                address="somewhere"
-              />
-              <LocationCard
-                title="skku"
-                photo="photo"
-                description="blabla"
-                address="somewhere"
-              />
-            </ScrollArea>
-          </SheetContent>
-        </Sheet>
+      <div className="h-96 w-full bg-neutral-400" />
+      <div className="border-1 flex flex-col justify-center gap-3 rounded-2xl border border-neutral-100 p-2">
+        <CardList />
+        <LocationCard
+          title="Bongsuyuk"
+          photo="photohere"
+          description="mookup"
+          address="~~ "
+        />
+        <LocationCard
+          title="skku"
+          photo="photo"
+          description="blabla"
+          address="somewhere"
+        />
       </div>
     </div>
+  );
+}
+
+function CardList() {
+  return (
+    <Drawer>
+      <DrawerTrigger className="self-center">
+        <BsChevronDoubleUp className="text-neutral-600" />
+      </DrawerTrigger>
+      <DrawerContent className="w-dvw justify-center">
+        <DrawerHeader>
+          <DrawerTitle>{}</DrawerTitle>
+        </DrawerHeader>
+        <ScrollArea>
+          <div className="flex h-[80vh] w-full flex-col gap-5 px-5 pb-4">
+            <LocationCard
+              title="skku"
+              photo="photo"
+              description="blabla"
+              address="somewhere"
+            />
+            <LocationCard
+              title="skku"
+              photo="photo"
+              description="blabla"
+              address="somewhere"
+            />
+            <LocationCard
+              title="skku"
+              photo="photo"
+              description="blabla"
+              address="somewhere"
+            />
+            <LocationCard
+              title="skku"
+              photo="photo"
+              description="blabla"
+              address="somewhere"
+            />
+            <LocationCard
+              title="skku"
+              photo="photo"
+              description="blabla"
+              address="somewhere"
+            />
+          </div>
+        </ScrollArea>
+      </DrawerContent>
+    </Drawer>
   );
 }
