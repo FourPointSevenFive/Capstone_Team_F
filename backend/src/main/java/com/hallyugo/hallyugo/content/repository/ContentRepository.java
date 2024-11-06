01a70db4b79e4ae1278835cdc78876166161250b
jisukimtech <jisukim.tech@gmail.com>
2024-11-06 23:48:11 +0900

    feature(be) - add repository method to fetch random contents by category

--- backend/src/main/java/com/hallyugo/hallyugo/content/repository/ContentRepository.java
+++ backend/src/main/java/com/hallyugo/hallyugo/content/repository/ContentRepository.java
@@ -1,7 +1,14 @@
 package com.hallyugo.hallyugo.content.repository;
 
+import com.hallyugo.hallyugo.content.domain.Category;
 import com.hallyugo.hallyugo.content.domain.Content;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
 
 public interface ContentRepository extends JpaRepository<Content, Long> {
+    @Query("SELECT c FROM Content c WHERE c.category = :category ORDER BY RAND()")
+    List<Content> findRandomContentsByCategory(@Param("category") Category category, Pageable pageable);
 }
