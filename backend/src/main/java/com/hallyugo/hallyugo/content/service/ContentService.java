29d8b8b4a2f0c8b20995333e874941d4fa278545
jisukimtech <jisukim.tech@gmail.com>
2024-11-07 03:46:45 +0900

    feature(be) - implement service logic to retrieve random contents from each category

---
+++ backend/src/main/java/com/hallyugo/hallyugo/content/service/ContentService.java
@@ -0,0 +1,37 @@
+package com.hallyugo.hallyugo.content.service;
+
+import com.hallyugo.hallyugo.content.domain.Category;
+import com.hallyugo.hallyugo.content.domain.Content;
+import com.hallyugo.hallyugo.content.domain.ContentResponseDto;
+import com.hallyugo.hallyugo.content.repository.ContentRepository;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class ContentService {
+    private static final int PAGE_NUMBER = 0;
+    private static final int INITIAL_CONTENTS_SIZE_PER_CATEGORY = 2;
+    private final ContentRepository contentRepository;
+
+    public Map<String, List<ContentResponseDto>> getRandomContentsByCategory() {
+        Map<String, List<ContentResponseDto>> result = new HashMap<>();
+        Pageable pageable = PageRequest.of(PAGE_NUMBER, INITIAL_CONTENTS_SIZE_PER_CATEGORY);
+
+        for (Category category : Category.values()) {
+            List<Content> contents = contentRepository.findRandomContentsByCategory(category, pageable);
+            List<ContentResponseDto> contentResponseDtos = contents.stream()
+                    .map(ContentResponseDto::toDto)
+                    .toList();
+
+            result.put(category.name(), contentResponseDtos);
+        }
+
+        return result;
+    }
+}
