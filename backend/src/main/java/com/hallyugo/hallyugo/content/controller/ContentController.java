99157e22727bcaf15df8e10499d9556c2f63a928
jisukimtech <jisukim.tech@gmail.com>
2024-11-07 03:49:06 +0900

    feature(be) - add endpoint in controller to get initial random contents by category

---
+++ backend/src/main/java/com/hallyugo/hallyugo/content/controller/ContentController.java
@@ -0,0 +1,24 @@
+package com.hallyugo.hallyugo.content.controller;
+
+import com.hallyugo.hallyugo.content.domain.ContentResponseDto;
+import com.hallyugo.hallyugo.content.service.ContentService;
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/content")
+@RestController
+public class ContentController {
+    private final ContentService contentService;
+
+    @GetMapping("/initial")
+    public ResponseEntity<Map<String, List<ContentResponseDto>>> getRandomContents() {
+        Map<String, List<ContentResponseDto>> result = contentService.getRandomContentsByCategory();
+        return ResponseEntity.ok(result);
+    }
+}
