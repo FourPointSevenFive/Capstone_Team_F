71716b7324a49bfbae78d48cd5d6c0ce98ae32c2
youznn <leejin914418@gmail.com>
2024-11-06 21:00:48 +0900

    feature(fe) - map page design

--- frontend/app/map/page.tsx
+++ frontend/app/map/page.tsx
@@ -1,29 +1,81 @@
 import Header from "@/app/_components/Header";
 import SearchBar from "../_components/SearchBar";
 import LocationCard from "./_components/LocationCard";
+import CustomBadge from "../_components/CustomBadge";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function Page() {
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       <Header />
       <div className="flex justify-between">
-        <div>badge</div>
+        <CustomBadge title="BTS" category="kpop" />
         <SearchBar />
       </div>
       <div className="flex flex-col gap-3">
-        map list
-        <LocationCard
-          title="Bongsuyuk"
-          photo="photohere"
-          description="mookup"
-          address="~~ "
-        />
-        <LocationCard
-          title="skku"
-          photo="photo"
-          description="blabla"
-          address="somewhere"
-        />
+        <div>
+          <LocationCard
+            title="Bongsuyuk"
+            photo="photohere"
+            description="mookup"
+            address="~~ "
+          />
+          <LocationCard
+            title="skku"
+            photo="photo"
+            description="blabla"
+            address="somewhere"
+          />
+        </div>
+
+        <Sheet>
+          <SheetTrigger>trigger</SheetTrigger>
+          <SheetHeader></SheetHeader>
+          <SheetTitle>Sheet Title</SheetTitle>
+          <SheetDescription>Sheet Description</SheetDescription>
+          <SheetContent side="bottom">
+            <ScrollArea className="h-[90vh]">
+              <LocationCard
+                title="skku"
+                photo="photo"
+                description="blabla"
+                address="somewhere"
+              />
+              <LocationCard
+                title="skku"
+                photo="photo"
+                description="blabla"
+                address="somewhere"
+              />
+              <LocationCard
+                title="skku"
+                photo="photo"
+                description="blabla"
+                address="somewhere"
+              />
+              <LocationCard
+                title="skku"
+                photo="photo"
+                description="blabla"
+                address="somewhere"
+              />
+              <LocationCard
+                title="skku"
+                photo="photo"
+                description="blabla"
+                address="somewhere"
+              />
+            </ScrollArea>
+          </SheetContent>
+        </Sheet>
       </div>
     </div>
   );
