fa7dd64ddf3899f814eddd99a092581b4610db59
jisukimtech <jisukim.tech@gmail.com>
2024-11-07 20:27:18 +0900

    test(be) - improve variable names and extract setup logic in service test

--- backend/src/test/java/com/hallyugo/hallyugo/content/service/ContentServiceTest.java
+++ backend/src/test/java/com/hallyugo/hallyugo/content/service/ContentServiceTest.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.redisson.api.RedissonClient;
@@ -17,6 +18,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
+@Transactional
 @ActiveProfiles("test")
 @SpringBootTest
 class ContentServiceTest {
@@ -32,31 +34,36 @@ class ContentServiceTest {
     @Autowired
     private ContentService contentService;
 
-    @Transactional
-    @DisplayName("각 카테고리마다 2개씩 총 8개의 랜덤 콘텐츠가 조회되어야 한다.")
-    @Test
-    void 카테고리별_랜덤_콘텐츠_조회_성공_테스트() {
-        // given
+    @BeforeEach
+    void setUp() {
         List<Content> contents = new ArrayList<>();
         contents.add(new Content(Category.DRAMA, "dramaTitle1", "dramaDesc1", "dramaUrl1"));
         contents.add(new Content(Category.DRAMA, "dramaTitle2", "dramaDesc2", "dramaUrl2"));
+        contents.add(new Content(Category.DRAMA, "dramaTitle3", "dramaDesc3", "dramaUrl3"));
         contents.add(new Content(Category.K_POP, "kpopTitle1", "kpopDesc1", "kpopUrl1"));
         contents.add(new Content(Category.K_POP, "kpopTitle2", "kpopDesc2", "kpopUrl2"));
+        contents.add(new Content(Category.K_POP, "kpopTitle3", "kpopDesc3", "kpopUrl3"));
         contents.add(new Content(Category.MOVIE, "movieTitle1", "movieDesc1", "movieUrl1"));
         contents.add(new Content(Category.MOVIE, "movieTitle2", "movieDesc2", "movieUrl2"));
+        contents.add(new Content(Category.MOVIE, "movieTitle3", "movieDesc3", "movieUrl3"));
         contents.add(new Content(Category.NOVEL, "novelTitle1", "novelDesc1", "novelUrl1"));
         contents.add(new Content(Category.NOVEL, "novelTitle2", "novelDesc2", "novelUrl2"));
+        contents.add(new Content(Category.NOVEL, "novelTitle3", "novelDesc3", "novelUrl3"));
 
         contentRepository.saveAll(contents);
+    }
 
+    @DisplayName("각 카테고리마다 2개씩 총 8개의 랜덤 콘텐츠가 조회되어야 한다.")
+    @Test
+    void 카테고리별_랜덤_콘텐츠_조회_성공_테스트() {
         // when
-        Map<String, List<ContentResponseDto>> fetchedContents = contentService.getRandomContentsByCategory();
+        Map<String, List<ContentResponseDto>> fetchedResult = contentService.getRandomContentsByCategory();
 
         // then
-        Assertions.assertThat(fetchedContents).hasSize(CATEGORY_COUNT);
+        Assertions.assertThat(fetchedResult).hasSize(CATEGORY_COUNT);
 
         for (Category category : Category.values()) {
-            Assertions.assertThat(fetchedContents.get(category.name())).hasSize(INITIAL_CONTENTS_SIZE_PER_CATEGORY);
+            Assertions.assertThat(fetchedResult.get(category.name())).hasSize(INITIAL_CONTENTS_SIZE_PER_CATEGORY);
         }
     }
 }
