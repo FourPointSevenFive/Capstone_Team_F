2dc39e0185b76d752dfad13ed5ba8093d3098669
jisukimtech <jisukim.tech@gmail.com>
2024-11-08 04:51:47 +0900

    feature(be) - throw EntityNotFoundException in service layer
    - Extracted service logic to improve readability.

--- backend/src/main/java/com/hallyugo/hallyugo/content/service/ContentService.java
+++ backend/src/main/java/com/hallyugo/hallyugo/content/service/ContentService.java
@@ -1,5 +1,7 @@
 package com.hallyugo.hallyugo.content.service;
 
+import com.hallyugo.hallyugo.common.exception.EntityNotFoundException;
+import com.hallyugo.hallyugo.common.exception.ExceptionCode;
 import com.hallyugo.hallyugo.content.domain.Category;
 import com.hallyugo.hallyugo.content.domain.Content;
 import com.hallyugo.hallyugo.content.domain.ContentResponseDto;
@@ -19,29 +21,45 @@ public class ContentService {
     private static final int INITIAL_CONTENTS_SIZE_PER_CATEGORY = 2;
     private final ContentRepository contentRepository;
 
-    public Map<String, List<ContentResponseDto>> getRandomContentsByCategory() {
+    public Map<String, List<ContentResponseDto>> getRandomContents() {
         Map<String, List<ContentResponseDto>> result = new HashMap<>();
         Pageable pageable = PageRequest.of(PAGE_NUMBER, INITIAL_CONTENTS_SIZE_PER_CATEGORY);
 
         for (Category category : Category.values()) {
-            List<Content> contents = contentRepository.findRandomContentsByCategory(category, pageable);
-            List<ContentResponseDto> contentResponseDtos = contents.stream()
-                    .map(ContentResponseDto::toDto)
-                    .toList();
-
+            List<ContentResponseDto> contentResponseDtos = getRandomContentsByCategory(category, pageable);
             result.put(category.name(), contentResponseDtos);
         }
 
         return result;
     }
 
+    private List<ContentResponseDto> getRandomContentsByCategory(Category category, Pageable pageable) {
+        List<Content> contents = contentRepository.findRandomContentsByCategory(category, pageable);
+
+        if (contents.isEmpty()) {
+            throw new EntityNotFoundException(ExceptionCode.ENTITY_NOT_FOUND);
+        }
+
+        return contents.stream().map(ContentResponseDto::toDto).toList();
+    }
+
     public List<ContentResponseDto> getContentsByCategory(String category) {
         List<Content> contents = contentRepository.findByCategory(Category.valueOf(category));
+
+        if (contents.isEmpty()) {
+            throw new EntityNotFoundException(ExceptionCode.ENTITY_NOT_FOUND);
+        }
+
         return contents.stream().map(ContentResponseDto::toDto).toList();
     }
 
     public List<ContentResponseDto> getContentsByKeyword(String keyword) {
         List<Content> contents = contentRepository.findByTitleContainingIgnoreCase(keyword);
+
+        if (contents.isEmpty()) {
+            throw new EntityNotFoundException(ExceptionCode.ENTITY_NOT_FOUND);
+        }
+
         return contents.stream().map(ContentResponseDto::toDto).toList();
     }
 }
