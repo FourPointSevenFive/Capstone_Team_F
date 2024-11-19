87474ea1db1ec246ca8bd97ea2a4169b750f4307
youznn <leejin914418@gmail.com>
2024-11-19 23:54:43 +0900

    feature(fe) - implement landing page ui

--- frontend/app/(auth)/layout.tsx
+++ frontend/app/(auth)/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { IoIosArrowBack } from "react-icons/io";
 
@@ -8,7 +9,10 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col gap-10">
       <div className="flex items-center justify-between">
-        <IoIosArrowBack className="size-8 cursor-pointer" />
+        <Link href="./landing">
+          <IoIosArrowBack className="size-8 cursor-pointer" />
+        </Link>
+
         <div>{path == "/login" ? "Sign In" : "Sign Up"}</div>
       </div>
       {children}
