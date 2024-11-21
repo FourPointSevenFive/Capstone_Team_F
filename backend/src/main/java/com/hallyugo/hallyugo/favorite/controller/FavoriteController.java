6bbf197a26c5c2ddaa0f236005fbeadfdf85b0ca
jisukimtech <jisukim.tech@gmail.com>
2024-11-22 02:50:19 +0900

    feature(be) - add endpoint to handle request for decreasing favorite count

--- backend/src/main/java/com/hallyugo/hallyugo/favorite/controller/FavoriteController.java
+++ backend/src/main/java/com/hallyugo/hallyugo/favorite/controller/FavoriteController.java
@@ -6,6 +6,7 @@ import com.hallyugo.hallyugo.favorite.service.FavoriteService;
 import com.hallyugo.hallyugo.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,6 +36,15 @@ public class FavoriteController {
             @PathVariable Long locationId
     ) {
         favoriteService.increaseFavoriteCountAndSave(user, locationId);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/favorite/off?location_id={locationId}")
+    public ResponseEntity<Void> decreaseFavoriteCount(
+            @AuthUser User user,
+            @PathVariable Long locationId
+    ) {
+        favoriteService.decreaseFavoriteCountAndDelete(user, locationId);
         return ResponseEntity.ok().build();
     }
 }
