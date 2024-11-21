ad389d8968b8c6e2388ad81a353ccdc0a15fe863
jisukimtech <jisukim.tech@gmail.com>
2024-11-22 01:54:46 +0900

    feature(be) - add repository method to check if Favorite exists for User and Location

--- backend/src/main/java/com/hallyugo/hallyugo/favorite/repository/FavoriteRepository.java
+++ backend/src/main/java/com/hallyugo/hallyugo/favorite/repository/FavoriteRepository.java
@@ -1,10 +1,11 @@
 package com.hallyugo.hallyugo.favorite.repository;
 
 import com.hallyugo.hallyugo.favorite.domain.Favorite;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
     List<Favorite> findByUserId(Long userId);
+
+    boolean existsByUserIdAndLocationId(Long userId, Long locationId);
 }
