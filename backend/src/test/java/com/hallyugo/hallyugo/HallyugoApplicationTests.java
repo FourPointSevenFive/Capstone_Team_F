b593341c5bc1e24bfcdfe2f97d61ce9900720dc2
jisukimtech <jisukim.tech@gmail.com>
2024-11-28 00:59:49 +0900

    test(be) - set up test profile using ActiveProfiles annotation

--- backend/src/test/java/com/hallyugo/hallyugo/HallyugoApplicationTests.java
+++ backend/src/test/java/com/hallyugo/hallyugo/HallyugoApplicationTests.java
@@ -2,12 +2,14 @@ package com.hallyugo.hallyugo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class HallyugoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
