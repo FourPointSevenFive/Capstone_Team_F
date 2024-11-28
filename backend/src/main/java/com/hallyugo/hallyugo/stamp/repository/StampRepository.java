57c95f390993210ab1939b189752711eac3113de
jisukimtech <jisukim.tech@gmail.com>
2024-11-29 03:16:46 +0900

    feature(be) - add repository method to find Stamp with User and Location

--- backend/src/main/java/com/hallyugo/hallyugo/stamp/repository/StampRepository.java
+++ backend/src/main/java/com/hallyugo/hallyugo/stamp/repository/StampRepository.java
@@ -1,12 +1,13 @@
 package com.hallyugo.hallyugo.stamp.repository;
 
 import com.hallyugo.hallyugo.stamp.domain.Stamp;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface StampRepository extends JpaRepository<Stamp, Long> {
     List<Stamp> findByUserId(Long userId);
+
+    List<Stamp> findByUserIdAndLocationId(Long userId, Long locationId);
 }
