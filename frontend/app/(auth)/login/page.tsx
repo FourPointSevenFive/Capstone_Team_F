740b01818f91d63613cd5be27598a031afa64fcc
youznn <leejin914418@gmail.com>
2024-11-30 00:33:07 +0900

    chore(fe) - login api

--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -1,18 +1,66 @@
+"use client";
+
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-
+import { signIn } from "next-auth/react";
 
 export default function SignInPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const onSubmit = async (event: React.FormEvent): Promise<void> => {
+    event.preventDefault();
+
+    try {
+      const res = await signIn("credentials", {
+        username,
+        password,
+      });
+
+      if (res && res.ok) {
+        window.location.href = "./";
+      } else {
+        setError("Failed to sign in. Please check your credentials.");
+      }
+    } catch (error) {
+      console.error("Failed to sign in: ", error);
+      setError("An unexpected error occurred. Please try again.");
+    }
+  };
+
   return (
     <div className="flex flex-col gap-8 px-8">
-      <Input placeholder="Email" type="email" className="h-12 bg-slate-100" />
-      <Input
-        placeholder="Password"
-        type="password"
-        className="h-12 bg-slate-100"
-      />
-      <Button className="h-12 bg-blue-500 hover:bg-blue-300">Sign In</Button>
-      <Button variant={"ghost"}>Create An Account</Button>
+      <form onSubmit={onSubmit} className="flex flex-col gap-4">
+        <Input
+          placeholder="Email"
+          type="text"
+          className="h-12 bg-slate-100"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <Input
+          placeholder="Password"
+          type="password"
+          className="h-12 bg-slate-100"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button
+          type="submit"
+          className="h-12 bg-blue-500 text-white hover:bg-blue-300"
+        >
+          Sign In
+        </Button>
+        <Button
+          variant="ghost"
+          onClick={() => (window.location.href = "/signup")}
+        >
+          Create An Account
+        </Button>
+        {error && <p className="text-red-500">{error}</p>}
+      </form>
     </div>
   );
 }
