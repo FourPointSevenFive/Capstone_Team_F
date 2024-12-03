0a8ab8f22a3bc24d672a00be94115faa16e9001a
jisukimtech <jisukim.tech@gmail.com>
2024-12-03 09:16:09 +0900

    feature(be) - add endpoint in Content controller to get Content with Locations and Images

--- backend/src/main/java/com/hallyugo/hallyugo/content/controller/ContentController.java
+++ backend/src/main/java/com/hallyugo/hallyugo/content/controller/ContentController.java
@@ -1,5 +1,6 @@
 package com.hallyugo.hallyugo.content.controller;
 
+import com.hallyugo.hallyugo.content.domain.response.ContentForMapResponseDto;
 import com.hallyugo.hallyugo.content.domain.response.ContentResponseDto;
 import com.hallyugo.hallyugo.content.service.ContentService;
 import java.util.List;
@@ -12,26 +13,34 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/content")
+@RequestMapping("/api/v1/")
 @RestController
 public class ContentController {
     private final ContentService contentService;
 
-    @GetMapping("/initial")
+    @GetMapping("/content/initial")
     public ResponseEntity<Map<String, List<ContentResponseDto>>> getRandomContents() {
         Map<String, List<ContentResponseDto>> result = contentService.getRandomContents();
         return ResponseEntity.ok(result);
     }
 
-    @GetMapping(params = "category")
+    @GetMapping(value = "/content", params = "category")
     public ResponseEntity<List<ContentResponseDto>> getContentsByCategory(@RequestParam String category) {
         List<ContentResponseDto> result = contentService.getContentsByCategory(category);
         return ResponseEntity.ok(result);
     }
 
-    @GetMapping(params = "keyword")
+    @GetMapping(value = "/content", params = "keyword")
     public ResponseEntity<List<ContentResponseDto>> getContentsByKeyword(@RequestParam String keyword) {
         List<ContentResponseDto> result = contentService.getContentsByKeyword(keyword);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping(value = "/location?content")
+    public ResponseEntity<ContentForMapResponseDto> getContentWithLocationsAndImages(
+            @RequestParam(name = "content_id") Long contentId
+    ) {
+        ContentForMapResponseDto result = contentService.getContentWithLocationsAndImages(contentId);
         return ResponseEntity.ok(result);
     }
 }
