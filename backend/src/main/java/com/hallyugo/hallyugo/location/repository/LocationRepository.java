a083fd467a608d9426ad3058f645345bb0fc9244
jisukimtech <jisukim.tech@gmail.com>
2024-12-03 09:02:27 +0900

    feature(be) - add Location repository method to find Location by Content id

--- backend/src/main/java/com/hallyugo/hallyugo/location/repository/LocationRepository.java
+++ backend/src/main/java/com/hallyugo/hallyugo/location/repository/LocationRepository.java
@@ -1,7 +1,9 @@
 package com.hallyugo.hallyugo.location.repository;
 
 import com.hallyugo.hallyugo.location.domain.Location;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
+    List<Location> findByContentId(Long contentId);
 }
