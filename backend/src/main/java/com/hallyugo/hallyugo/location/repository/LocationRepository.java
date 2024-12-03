27f85e28a6ac40f82a27eb085ac5dcccb4501e2d
jisukimtech <jisukim.tech@gmail.com>
2024-12-03 18:20:13 +0900

    feature(be) - add Location repository method to find Location by keyword

--- backend/src/main/java/com/hallyugo/hallyugo/location/repository/LocationRepository.java
+++ backend/src/main/java/com/hallyugo/hallyugo/location/repository/LocationRepository.java
@@ -5,5 +5,8 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
+
     List<Location> findByContentId(Long contentId);
+
+    List<Location> findByTitleContainingIgnoreCase(String keyword);
 }
